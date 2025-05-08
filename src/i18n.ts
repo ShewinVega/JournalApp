73a6f42982a9173edfb3ac5fe09628507cfccd19
ShewinVega <edwinvega.aid@gmail.com>
2025-05-07 19:58:41 -0600

    refactor: i18n configuration was refactored.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,21 @@
-import i18n from "i18next";
+import i18n, { TFunction } from "i18next";
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 const getCurrentHost: string =
   import.meta.env.MODE === "development"
     ? "http://localhost:5173"
     : "LINK TO PRODUCTION";
 
-const initI18nPromise = i18n
+// Check the localStorage before i18next can
+const storageLang: string | null = localStorage.getItem("i18nextLng");
+
+const initI18nPromise: Promise<TFunction<"translation", undefined>> = i18n
+  .use(LanguageDetector)
   .use(i18nBackend)
   .use(initReactI18next)
   .init({
-    lng: localStorage.getItem("lng") || "es",
     fallbackLng: "es",
     interpolation: {
       escapeValue: false,
@@ -19,6 +23,7 @@ const initI18nPromise = i18n
     backend: {
       loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
     },
+    storageLang,
   });
 
 export { initI18nPromise };
